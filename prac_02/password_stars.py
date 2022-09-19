9c34bef36509ebd70c7f25d2c3648d841fee64ac
JacobOToole <jacob.otoole@my.jcu.edu.au>
2022-09-19 15:50:53 +1000

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,23 @@
-minimum_length = 5
 
-password = input("Password: ")
-while len(password) < minimum_length:
-    print("Password is too short")
+
+def main():
+    minimum_length = 5
+
+    password = get_password(minimum_length)
+
+    print_password(password)
+
+
+def print_password(password):
+    print('*' * len(password))
+
+
+def get_password(minimum_length):
     password = input("Password: ")
+    while len(password) < minimum_length:
+        print("Password is too short")
+        password = input("Password: ")
+    return password
 
-print('*' * len(password))
+
+main()
