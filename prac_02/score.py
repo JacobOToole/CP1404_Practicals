940ffe0d1ac738ef1f775b521b3f3afaa181f83b
JacobOToole <jacob.otoole@my.jcu.edu.au>
2022-09-23 10:55:34 +1000

    Refactor score evaluator program to include functions and random score feature

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,26 +3,28 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+def main():
+    score = float(input("Enter score: "))
+    result = calculate_result(score)
+    print(result)
+    random_score = random.randint(0, 100)
+    result = calculate_result(random_score)
+    print(f"Random score: {random_score}")
+    print(result)
 
 
-# score = float(input("Enter score: "))
-# if score < 0:
-#     print("Invalid score")
-# else:
-#     if score > 100:
-#         print("Invalid score")
-#     if score > 50:
-#         print("Passable")
-#     if score > 90:
-#         print("Excellent")
-# if score < 50:
-#     print("Bad")
+def calculate_result(score):
+    if score < 0 or score > 100:
+        result = 'Invalid score'
+    elif score >= 90:
+        result = 'Excellent'
+    elif score >= 50:
+        result = 'Passable'
+    else:
+        result = 'Bad'
+    return result
+
+
+main()
