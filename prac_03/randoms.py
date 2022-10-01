aaa1fb035f528cdf3bc13ea4b9bb5198fdde7dfa
JacobOToole <jacob.otoole@my.jcu.edu.au>
2022-10-01 16:30:47 +1000

    Add random.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,10 @@
+# 1. 18, 18, 18, 20, 13.
+#     smallest = 5, largest = 20
+#     line 2 could nto have produced 4
+# 2. 3, 9, 7, 7, 3
+#     smallest = 3, largest = 9
+# 3. 5.334017104689983, 5.200080391383158, 3.007908148214405
+#     smallest = 2.5, largest = 5.5
+import random
+
+print(random.randint(1, 100))
