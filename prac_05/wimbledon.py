1d03eb16baf50d0d36c3dfdb069bab0d50e702ba
JacobOToole <jacob.otoole@my.jcu.edu.au>
2022-10-17 17:28:48 +1000

    Add wimbledon.py

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+
+def main():
+
+
+
+def read_champions():
+    with open('wimbledon.csv', "r", encoding="utf-8-sig") as in_file:
+    #     amend {champions} with players and how many times they repeat
+    #     amend {champion countries} in alphabetical
+
+
+def display_champions(champions, champion_countries):
+    print("Wimbledon Champions:")
+    for champion in champions:
+        print(f"{champion}, {amount_won}")
+    # probably use dictionary there with champion as key and amount as value
+    print(f"These {len(champion_countries)} have won Wimbleton: \n{champion_countries}")
+
+
+main()
