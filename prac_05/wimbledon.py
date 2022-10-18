eaf2b5e0af63bfbb86f10c024894b6fef87f4ce8
JacobOToole <jacob.otoole@my.jcu.edu.au>
2022-10-18 15:19:42 +1000

    Add wimbledon.py

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,22 +2,41 @@
 
 """
 
-def main():
 
+def main():
+    champions = read_champions()
+    champion_countries = read_champion_countries()
+    display_champions(champions, champion_countries)
 
 
 def read_champions():
+    champions_list = []
     with open('wimbledon.csv', "r", encoding="utf-8-sig") as in_file:
-    #     amend {champions} with players and how many times they repeat
-    #     amend {champion countries} in alphabetical
+        for line in in_file:
+            detail_list = line.split(',')
+            champions_list.append(detail_list[2])
+            champions = {i: champions_list.count(i) for i in champions_list}
+        champions.pop("Champion")
+        return champions
+
+
+def read_champion_countries():
+    champion_country_set = set()
+    with open('wimbledon.csv', "r", encoding="utf-8-sig") as in_file:
+        for line in in_file:
+            detail_list = line.split(',')
+            champion_country_set.add(detail_list[3])
+    champion_country_set.remove('Country')
+    sorted(champion_country_set)
+    return champion_country_set
 
 
 def display_champions(champions, champion_countries):
     print("Wimbledon Champions:")
     for champion in champions:
-        print(f"{champion}, {amount_won}")
-    # probably use dictionary there with champion as key and amount as value
-    print(f"These {len(champion_countries)} have won Wimbleton: \n{champion_countries}")
+        print(f"{champion} {champions[champion]}")
+    print(f"These {len(champion_countries)} countries have won Wimbledon:")
+    print(", ".join(champion_countries))
 
 
 main()
