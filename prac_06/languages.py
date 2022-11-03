e4a50fa7372ebbdda6333a2daf1e29d170af1f2a
JacobOToole <jacob.otoole@my.jcu.edu.au>
2022-11-03 17:12:52 +1000

    Add Intermediate Exercise

---
+++ prac_06/languages.py
@@ -0,0 +1,24 @@
+"""
+CP1404 - Trimester 3 2022 - Jacob O'Toole
+Programming language exercise
+estimated: 30 minutes
+actual: 25 minutes
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+    languages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
